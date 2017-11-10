c41b023c98a6426a41c208b4681807e0906635aa
Austin Phillips <phillips.austin51@gmail.com>
2017-11-10 16:29:51 -0700

    Put colors

--- 8_ball.rb
+++ 8_ball.rb
@@ -15,22 +15,23 @@ def menu
 end
 
 def main
-  puts "Welcome to Magic Eight-ball"
-  puts "Ask me anything"
+  puts "Welcome to Magic Eight-ball" .cyan
+  puts "Ask me anything" .cyan
   gets.strip.to_s
   answer
 end
 
 def answer
-  puts "I'm thinking..."
+  puts "I'm thinking..." .cyan
   puts @answers.sample .green
-  puts "Type quit or just ask another question."
+  puts "Type quit or just ask another question." .cyan
   choice
 end
 
 def choice
-  response = gets.strip.to_s
+  response = gets.strip.to_s.downcase
   if response == "quit"
+    puts "Thanks for using the magic 8-ball" .red
     exit
   else
     answer
