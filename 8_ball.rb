18cd67c6298b7a912e68f26409afd6bc35537434
Austin Phillips <phillips.austin51@gmail.com>
2017-11-10 16:00:47 -0700

    Working Frame

--- 8_ball.rb
+++ 8_ball.rb
@@ -1,2 +1,37 @@
-Puts "class"
-puts "help"
+@answers = [
+  "Follow your heart",
+  "Go for it",
+  "Dont be a dumbass",
+  "Better luck next",
+  "Nope",
+  "Never gonna happen"
+]
+
+def menu
+  main
+end
+
+def main
+  puts "Welcome to Magic Eight-ball"
+  puts "Ask me anything"
+  gets.strip.to_s
+  answer
+end
+
+def answer
+  puts "I'm thinking..."
+  puts "#{@answers.sample}"
+  puts "Type quit or just ask another question."
+  choice
+end
+
+def choice
+  response = gets.strip.to_s
+  if response == "quit"
+    exit
+  else
+    answer
+  end
+end
+
+menu
